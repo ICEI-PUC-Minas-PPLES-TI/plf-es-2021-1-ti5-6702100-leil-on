039af74be49c5062c8cc7e2369799d7dcd41218c
PHMSilva <magengsoft@gmail.com>
2021-04-04 20:03:28 -0300

    criacao do menu do dashboard

--- codigo/front-end/Leil_on/lib/view/dashBoardUser.dart
+++ codigo/front-end/Leil_on/lib/view/dashBoardUser.dart
@@ -1,3 +1,6 @@
+import 'dart:ui';
+
+import 'package:Leil_on/widget/menuLeilon.dart';
 import 'package:flutter/material.dart';
 
 class Saudacao extends StatelessWidget {
@@ -7,9 +10,17 @@ class Saudacao extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Tela DashBoard'),
+        centerTitle: true,
+        iconTheme: IconThemeData(color: Colors.purple, size: 50.0),
+        title: Text(
+          'Tela DashBoard',
+          style: TextStyle(
+            color: Colors.grey[100],
+          ),
+        ),
         backgroundColor: Colors.amber[400],
       ),
+      drawer: MenuLeilon(),
       body: Container(
         color: Colors.grey[200],
         child: Text(
---
+++ codigo/front-end/Leil_on/lib/widget/menuLeilon.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class MenuLeilon extends StatelessWidget {
+  const MenuLeilon({Key key}) : super(key: key);
+
+  Widget _createItem(IconData icon, String label) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(color: Colors.grey[50]),
+        ),
+      ),
+      child: ListTile(
+        leading: Icon(
+          icon,
+          size: 25,
+          color: Colors.purple,
+        ),
+        title: Text(
+          label,
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.grey[200],
+          ),
+        ),
+        onTap: () {},
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height * 0.4,
+            width: double.infinity,
+            padding: EdgeInsets.only(
+              top: 20,
+              left: 10,
+              right: 10,
+            ),
+            color: Colors.amber[400],
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Icon(
+                  Icons.person,
+                  color: Colors.deepPurple[800],
+                  size: 120.0,
+                ),
+              ],
+            ),
+          ),
+          Container(
+            color: Colors.amber[400],
+            child: Column(
+              children: <Widget>[
+                _createItem(Icons.add_to_photos, 'Criar leilão'),
+                _createItem(Icons.settings, 'Configurações'),
+                _createItem(Icons.chat, 'Chat'),
+                _createItem(Icons.exit_to_app, 'Sair'),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
